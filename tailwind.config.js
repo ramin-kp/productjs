8e2f58e161029d1c2abf9fbaaea0259cf93b469a
Ramin Karimpor <ramin.kp81@gmail.com>
2023-10-03 15:39:31 +0330

    feat: developing product section and styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,js}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        LarsseitLight: "Larsseit Light",
+        LarsseitMedium: "Larsseit Medium",
+        LarsseitBold: "Larsseit Bold",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          lg: "0.7rem",
+        },
+      },
+    },
+    screens: {
+      xs:"480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 };
